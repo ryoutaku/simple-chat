745a0a9eb29685f6d8d314cc3cd78f897b9341ba
ryoutaku nakagawa <uoyrutakoys@gmail.com>
2021-01-05 03:10:14 +0900

    [ADD]create_server

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr: "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
