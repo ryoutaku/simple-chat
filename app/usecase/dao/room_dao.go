6ce2f5ffe4ec6a670a50f48a9e11433f3be8d6c4
ryoutaku nakagawa <uoyrutakoys@gmail.com>
2021-02-17 23:18:52 +0900

    [ADD]RoomRepositoryのインターフェイス実装

--- app/usecase/dao/room_dao.go
+++ app/usecase/dao/room_dao.go
@@ -1,5 +1,8 @@
-package main
+package dao
 
-func main() {
-	$END$
+import "github.com/ryoutaku/simple-chat/app/domain"
+
+type RoomRepository interface {
+	All() (rooms domain.Rooms, err error)
+	Create(room *domain.Room) (err error)
 }
