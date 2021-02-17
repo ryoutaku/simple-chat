6974b43b194619456953e4b2383767a401662a65
ryoutaku nakagawa <uoyrutakoys@gmail.com>
2021-02-17 23:29:34 +0900

    [ADD]HttpContextの実装

--- app/infra/http_context.go
+++ app/infra/http_context.go
@@ -1,7 +1,8 @@
-package adapter
+package infra
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
@@ -10,14 +11,25 @@ type HttpContext struct {
 	Request *http.Request
 }
 
-func (c *HttpContext) Bind(i interface{}) error {
-	return json.NewDecoder(c.Request.Body).Decode(i)
+func NewHttpContext(w http.ResponseWriter, r *http.Request) *HttpContext {
+	return &HttpContext{
+		Writer:  w,
+		Request: r,
+	}
 }
 
-func (c *HttpContext) JSON(code int, i interface{}) error {
+func (c *HttpContext) Bind(i interface{}) (err error) {
+	err = json.NewDecoder(c.Request.Body).Decode(i)
+	if err != nil {
+		return errors.New("invalid request")
+	}
+	return err
+}
+
+func (c *HttpContext) JSON(code int, i interface{}) (err error) {
 	res, err := json.Marshal(i)
 	if err != nil {
-		return err
+		return errors.New("internal server error")
 	}
 
 	c.Writer.Header().Set("Content-Type", "application/json")
--- app/infra/http_context_test.go
+++ app/infra/http_context_test.go
@@ -1,31 +1,115 @@
 package infra
 
 import (
-	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"math"
 	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
 )
 
-type HttpContext struct {
-	Writer  http.ResponseWriter
-	Request *http.Request
+type dummyInputData struct {
+	ID   int
+	Name string
 }
 
-func newHttpContext(w http.ResponseWriter, r *http.Request) HttpContext {
-	return HttpContext{Writer: w, Request: r}
+type dummyResponseBody struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
-func (c *HttpContext) Bind(i interface{}) error {
-	return json.NewDecoder(c.Request.Body).Decode(i)
-}
-
-func (c *HttpContext) JSON(code int, i interface{}) error {
-	res, err := json.Marshal(i)
-	if err != nil {
-		return err
+func TestBind(t *testing.T) {
+	testCases := []struct {
+		name         string
+		body         string
+		expectedErr  error
+		expectedData *dummyInputData
+	}{
+		{
+			name:         "normal",
+			body:         `{"id":1,"name":"test"}`,
+			expectedErr:  nil,
+			expectedData: &dummyInputData{ID: 1, Name: "test"},
+		},
+		{
+			name:         "invalid request",
+			body:         `{"id":"test","name":1}`,
+			expectedErr:  errors.New("invalid request"),
+			expectedData: &dummyInputData{},
+		},
 	}
 
-	c.Writer.Header().Set("Content-Type", "application/json")
-	c.Writer.WriteHeader(code)
-	c.Writer.Write(res)
-	return err
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			body := strings.NewReader(tc.body)
+			r := httptest.NewRequest("Post", "/dummies", body)
+			w := httptest.NewRecorder()
+
+			context := NewHttpContext(w, r)
+			data := new(dummyInputData)
+
+			err := context.Bind(&data)
+			if !reflect.DeepEqual(tc.expectedErr, err) {
+				t.Errorf("context.Bind expected = %v, got = %v", tc.expectedErr, err)
+			}
+			if !reflect.DeepEqual(tc.expectedData, data) {
+				t.Errorf("context.Bind expected = %v, got = %v", tc.expectedData, data)
+			}
+		})
+	}
+}
+
+func TestJSON(t *testing.T) {
+	testCases := []struct {
+		name         string
+		body         interface{}
+		code         int
+		expectedErr  error
+		expectedBody string
+	}{
+		{
+			name:         "normal",
+			body:         dummyResponseBody{ID: 1, Name: "test"},
+			code:         http.StatusOK,
+			expectedErr:  nil,
+			expectedBody: "{\"id\":1,\"name\":\"test\"}",
+		},
+		{
+			name:         "json.Marshal Error",
+			body:         math.Inf(0),
+			code:         http.StatusOK,
+			expectedErr:  errors.New("internal server error"),
+			expectedBody: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			r := httptest.NewRequest("Get", "/dummy", nil)
+			w := httptest.NewRecorder()
+			context := NewHttpContext(w, r)
+
+			err := context.JSON(tc.code, tc.body)
+			if !reflect.DeepEqual(tc.expectedErr, err) {
+				t.Errorf("context.JSON expected = %v, got = %v", tc.expectedErr, err)
+			}
+
+			resp := w.Result()
+			if resp.StatusCode != tc.code {
+				t.Errorf("context.JSON StatusCode expected = %v, got = %v", tc.code, resp.StatusCode)
+			}
+
+			body, _ := ioutil.ReadAll(resp.Body)
+			if string(body) != tc.expectedBody {
+				t.Errorf("context.JSON ResponseBody expected = %v, got = %v", tc.expectedBody, string(body))
+			}
+		})
+	}
 }
