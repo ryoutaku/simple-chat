07974d4b7d4b46f7e8dfc748c931177212d6766d
ryoutaku nakagawa <uoyrutakoys@gmail.com>
2021-02-17 23:28:10 +0900

    [ADD]RoomControllerの実装

--- app/interface/repository/room_repository.go
+++ app/interface/repository/room_repository.go
@@ -3,35 +3,28 @@ package repository
 import (
 	"errors"
 
-	"github.com/go-playground/validator/v10"
+	"github.com/ryoutaku/simple-chat/app/interface/adapter"
+
 	"github.com/ryoutaku/simple-chat/app/domain"
-	"github.com/ryoutaku/simple-chat/app/interfaces/adapter"
-	"github.com/ryoutaku/simple-chat/app/usecase/storage"
 )
 
-var validate = validator.New()
-
-type roomRepository struct {
+type RoomRepository struct {
 	DBHandler adapter.DBHandler
 }
 
-func NewRoomRepository(db adapter.DBHandler) storage.RoomRepository {
-	return &roomRepository{DBHandler: db}
+func NewRoomRepository(db adapter.DBHandler) *RoomRepository {
+	return &RoomRepository{DBHandler: db}
 }
 
-func (r *roomRepository) All() (rooms domain.Rooms, err error) {
-	if e := r.DBHandler.Find(&rooms); e != nil {
+func (r *RoomRepository) All() (rooms domain.Rooms, err error) {
+	err = r.DBHandler.Find(&rooms)
+	if err != nil {
 		err = errors.New("not found")
 	}
 	return
 }
 
-func (r *roomRepository) Create(room *domain.Room) (err error) {
-	err = validate.Struct(room)
-	if err != nil {
-		return
-	}
-
+func (r *RoomRepository) Create(room *domain.Room) (err error) {
 	err = r.DBHandler.Create(room)
 	return
 }
--- app/interface/repository/room_repository_test.go
+++ app/interface/repository/room_repository_test.go
@@ -2,14 +2,31 @@ package repository
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
 
-	"github.com/ryoutaku/simple-chat/app/interfaces/adapter"
+	"github.com/ryoutaku/simple-chat/app/testdata/mocks"
 
 	"github.com/ryoutaku/simple-chat/app/domain"
 )
+
+var dummyRoom = domain.Room{
+	ID:   1,
+	Name: "テストルーム1",
+}
+
+var validateErrRoom = domain.Room{
+	ID: 1,
+}
+
+var expectedRoom = domain.Room{
+	ID:        1,
+	Name:      "テストルーム1",
+	CreatedAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+	UpdatedAt: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local),
+}
 
 var dummyRooms = domain.Rooms{
 	domain.Room{
@@ -37,63 +54,83 @@ func TestAll(t *testing.T) {
 			name:           "dbHandler.Find Error",
 			dbHandlerErr:   errors.New("dbHandler.Find Error"),
 			expectedReturn: domain.Rooms{},
-			expectedError:  errors.New("dbHandler.Find Error"),
+			expectedError:  errors.New("not found"),
 		},
 	}
 
-	for _, test := range testCases {
-		fakeDBHandler := adapter.FakeDBHandler{
-			FakeFind: func(dest interface{}, conds ...interface{}) error {
-				return test.dbHandlerErr
-			},
-		}
-		repository := NewRoomRepository(fakeDBHandler)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			fakeDBHandler := mocks.FakeDBHandler{
+				FakeFind: func(dest interface{}, conds ...interface{}) error {
+					if tc.dbHandlerErr == nil {
+						pv := reflect.ValueOf(dest)
+						vv := reflect.ValueOf(dummyRooms)
+						pv.Elem().Set(vv)
+					}
+					return tc.dbHandlerErr
+				},
+			}
+			repository := NewRoomRepository(fakeDBHandler)
 
-		rooms, err := repository.All()
-		if !reflect.DeepEqual(test.expectedError, err) {
-			t.Errorf("%v: repository.All error expected = %v, got = %v", test.name, test.expectedError, err)
-		}
-		if err == nil && !reflect.DeepEqual(test.expectedReturn, rooms) {
-			t.Errorf("%v: repository.All return expected = %v, got = %v", test.name, test.expectedReturn, rooms)
-		}
+			rooms, err := repository.All()
+			if !reflect.DeepEqual(tc.expectedError, err) {
+				t.Errorf("%v: repository.All error expected = %v, got = %v", tc.name, tc.expectedError, err)
+			}
+			if err == nil && !reflect.DeepEqual(tc.expectedReturn, rooms) {
+				t.Errorf("%v: repository.All return expected = %v, got = %v", tc.name, tc.expectedReturn, rooms)
+			}
+		})
 	}
 }
 
 func TestCreate(t *testing.T) {
 	testCases := []struct {
-		name           string
-		dbHandlerErr   error
-		expectedReturn domain.Rooms
-		expectedError  error
+		name          string
+		room          domain.Room
+		dbHandlerErr  error
+		expectedError error
 	}{
 		{
-			name:           "normal",
-			dbHandlerErr:   nil,
-			expectedReturn: dummyRooms,
-			expectedError:  nil,
+			name:          "normal",
+			room:          dummyRoom,
+			dbHandlerErr:  nil,
+			expectedError: nil,
 		},
 		{
-			name:           "dbHandler.Find Error",
-			dbHandlerErr:   errors.New("dbHandler.Find Error"),
-			expectedReturn: domain.Rooms{},
-			expectedError:  errors.New("dbHandler.Find Error"),
+			name:          "dbHandler.Find Error",
+			room:          dummyRoom,
+			dbHandlerErr:  errors.New("dbHandler.Create Error"),
+			expectedError: errors.New("dbHandler.Create Error"),
 		},
 	}
 
-	for _, test := range testCases {
-		fakeDBHandler := adapter.FakeDBHandler{
-			FakeCreate: func(value interface{}) error {
-				return test.dbHandlerErr
-			},
-		}
-		repository := NewRoomRepository(fakeDBHandler)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			fakeDBHandler := mocks.FakeDBHandler{
+				FakeCreate: func(value interface{}) error {
+					if tc.dbHandlerErr == nil {
+						pv := reflect.ValueOf(value)
+						vv := reflect.ValueOf(expectedRoom)
+						pv.Elem().Set(vv)
+					}
+					return tc.dbHandlerErr
+				},
+			}
+			repository := NewRoomRepository(fakeDBHandler)
 
-		rooms, err := repository.All()
-		if !reflect.DeepEqual(test.expectedError, err) {
-			t.Errorf("%v: repository.All error expected = %v, got = %v", test.name, test.expectedError, err)
-		}
-		if err == nil && !reflect.DeepEqual(test.expectedReturn, rooms) {
-			t.Errorf("%v: repository.All return expected = %v, got = %v", test.name, test.expectedReturn, rooms)
-		}
+			err := repository.Create(&tc.room)
+			if !reflect.DeepEqual(tc.expectedError, err) {
+				fmt.Println(tc.expectedError)
+				fmt.Println(err)
+				t.Errorf("repository.All error expected = %v, got = %v", tc.expectedError, err)
+			}
+			if err == nil && !reflect.DeepEqual(expectedRoom, tc.room) {
+				t.Errorf("repository.All return expected = %v, got = %v", expectedRoom, tc.room)
+			}
+		})
 	}
 }
