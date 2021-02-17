0d6734b8faf5e18bf5e9660382b231e8c30d9ccd
ryoutaku nakagawa <uoyrutakoys@gmail.com>
2021-02-17 23:27:45 +0900

    [ADD]RoomControllerの実装

--- app/interface/controller/room_controller.go
+++ app/interface/controller/room_controller.go
@@ -4,8 +4,9 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/ryoutaku/simple-chat/app/interfaces/adapter"
-	"github.com/ryoutaku/simple-chat/app/usecase/boundary"
+	"github.com/ryoutaku/simple-chat/app/interface/adapter"
+
+	"github.com/ryoutaku/simple-chat/app/usecase/input"
 )
 
 type roomResponse struct {
@@ -18,45 +19,45 @@ type roomResponse struct {
 type roomsResponse []roomResponse
 
 type RoomController struct {
-	Service boundary.RoomService
+	Service input.RoomService
 }
 
-func NewRoomController(s boundary.RoomService) *RoomController {
+func NewRoomController(s input.RoomService) *RoomController {
 	return &RoomController{Service: s}
 }
 
 func (c *RoomController) Index(hc adapter.HttpContext) *adapter.HttpError {
 	rooms, err := c.Service.All()
 	if err != nil {
-		return adapter.NewHttpError(err, http.StatusBadRequest)
+		return adapter.NewHttpError(err.Error(), http.StatusBadRequest)
 	}
 
 	respBody := convertRoomsResponse(&rooms)
 	if err := hc.JSON(http.StatusCreated, respBody); err != nil {
-		return adapter.NewHttpError(err, http.StatusInternalServerError)
+		return adapter.NewHttpError(err.Error(), http.StatusInternalServerError)
 	}
 	return nil
 }
 
 func (c *RoomController) Create(context adapter.HttpContext) *adapter.HttpError {
-	var inputData boundary.RoomInputData
+	var inputData input.RoomInputData
 	if err := context.Bind(&inputData); err != nil {
-		return adapter.NewHttpError(err, http.StatusBadRequest)
+		return adapter.NewHttpError(err.Error(), http.StatusBadRequest)
 	}
 
 	room, err := c.Service.Create(inputData)
 	if err != nil {
-		return adapter.NewHttpError(err, http.StatusBadRequest)
+		return adapter.NewHttpError(err.Error(), http.StatusBadRequest)
 	}
 
 	respBody := convertRoomResponse(&room)
 	if err = context.JSON(http.StatusCreated, respBody); err != nil {
-		return adapter.NewHttpError(err, http.StatusInternalServerError)
+		return adapter.NewHttpError(err.Error(), http.StatusInternalServerError)
 	}
 	return nil
 }
 
-func convertRoomResponse(room *boundary.RoomOutputData) (resp roomResponse) {
+func convertRoomResponse(room *input.RoomOutputData) (resp roomResponse) {
 	resp.ID = room.ID
 	resp.Name = room.Name
 	resp.CreatedAt = room.CreatedAt
@@ -64,7 +65,7 @@ func convertRoomResponse(room *boundary.RoomOutputData) (resp roomResponse) {
 	return
 }
 
-func convertRoomsResponse(rooms *boundary.RoomsOutputData) (resp roomsResponse) {
+func convertRoomsResponse(rooms *input.RoomsOutputData) (resp roomsResponse) {
 	for _, room := range *rooms {
 		var r roomResponse
 		r = convertRoomResponse(&room)
--- app/interface/controller/room_controller_test.go
+++ app/interface/controller/room_controller_test.go
@@ -6,82 +6,88 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ryoutaku/simple-chat/app/usecase/boundary"
+	"github.com/ryoutaku/simple-chat/app/interface/adapter"
 
-	"github.com/ryoutaku/simple-chat/app/interfaces/adapter"
+	"github.com/ryoutaku/simple-chat/app/testdata/mocks"
+
+	"github.com/ryoutaku/simple-chat/app/usecase/input"
 )
 
 type fakeRoomService struct {
-	boundary.RoomService
-	fakeAll    func() (outData boundary.RoomsOutputData, err error)
-	fakeCreate func(inData boundary.RoomInputData) (outData boundary.RoomOutputData, err error)
+	input.RoomService
+	fakeAll    func() (outData input.RoomsOutputData, err error)
+	fakeCreate func(inData input.RoomInputData) (outData input.RoomOutputData, err error)
 }
 
-func (i fakeRoomService) All() (outData boundary.RoomsOutputData, err error) {
+func (i fakeRoomService) All() (outData input.RoomsOutputData, err error) {
 	return i.fakeAll()
 }
 
-func (i fakeRoomService) Create(inData boundary.RoomInputData) (outData boundary.RoomOutputData, err error) {
+func (i fakeRoomService) Create(inData input.RoomInputData) (outData input.RoomOutputData, err error) {
 	return i.fakeCreate(inData)
 }
 
-var roomOutputData = boundary.RoomOutputData{
+var roomOutputData = input.RoomOutputData{
 	ID:        1,
 	Name:      "テストルーム1",
 	CreatedAt: time.Now(),
 	UpdatedAt: time.Now(),
 }
 
-var roomsOutputData = boundary.RoomsOutputData{
+var roomsOutputData = input.RoomsOutputData{
 	roomOutputData,
 }
 
 func TestRoomControllerIndex(t *testing.T) {
 	testCases := []struct {
 		name          string
-		serviceOutput boundary.RoomsOutputData
+		serviceReturn input.RoomsOutputData
 		serviceErr    error
 		jsonErr       error
 		expected      *adapter.HttpError
 	}{
 		{
 			name:          "normal",
-			serviceOutput: roomsOutputData,
+			serviceReturn: roomsOutputData,
 			serviceErr:    nil,
 			jsonErr:       nil,
 			expected:      nil,
 		},
 		{
-			name:          "Service Error",
-			serviceOutput: boundary.RoomsOutputData{},
-			serviceErr:    errors.New("error"),
+			name:          "serviceError",
+			serviceReturn: input.RoomsOutputData{},
+			serviceErr:    errors.New("serviceError"),
 			jsonErr:       nil,
-			expected:      adapter.NewHttpError(errors.New("error"), 400),
+			expected:      adapter.NewHttpError("serviceError", 400),
 		},
 		{
-			name:          "context.JSON Error",
-			serviceOutput: roomsOutputData,
+			name:          "jsonError",
+			serviceReturn: roomsOutputData,
 			serviceErr:    nil,
-			jsonErr:       errors.New("error"),
-			expected:      adapter.NewHttpError(errors.New("error"), 500),
+			jsonErr:       errors.New("jsonError"),
+			expected:      adapter.NewHttpError("jsonError", 500),
 		},
 	}
 
-	for _, test := range testCases {
-		context := adapter.FakeHttpContext{
-			FakeJSON: func(int, interface{}) error { return test.jsonErr },
-		}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			context := mocks.FakeHttpContext{
+				FakeJSON: func(int, interface{}) error { return tc.jsonErr },
+			}
 
-		service := fakeRoomService{
-			fakeAll: func() (outData boundary.RoomsOutputData, err error) {
-				return test.serviceOutput, test.serviceErr
-			},
-		}
-		controller := NewRoomController(service)
+			service := fakeRoomService{
+				fakeAll: func() (outData input.RoomsOutputData, err error) {
+					return tc.serviceReturn, tc.serviceErr
+				},
+			}
+			controller := NewRoomController(service)
 
-		if err := controller.Index(context); !reflect.DeepEqual(test.expected, err) {
-			t.Errorf("%v: controller.Index expected = %v, got = %v", test.name, test.expected, err)
-		}
+			if err := controller.Index(context); !reflect.DeepEqual(tc.expected, err) {
+				t.Errorf("%v: controller.Index expected = %v, got = %v", tc.name, tc.expected, err)
+			}
+		})
 	}
 }
 
@@ -89,7 +95,7 @@ func TestRoomControllerCreate(t *testing.T) {
 	testCases := []struct {
 		name          string
 		bindErr       error
-		serviceOutput boundary.RoomOutputData
+		serviceReturn input.RoomOutputData
 		serviceErr    error
 		jsonErr       error
 		expected      *adapter.HttpError
@@ -97,52 +103,56 @@ func TestRoomControllerCreate(t *testing.T) {
 		{
 			name:          "normal",
 			bindErr:       nil,
-			serviceOutput: roomOutputData,
+			serviceReturn: roomOutputData,
 			serviceErr:    nil,
 			jsonErr:       nil,
 			expected:      nil,
 		},
 		{
-			name:          "context.Bind Error",
-			bindErr:       errors.New("error"),
-			serviceOutput: boundary.RoomOutputData{},
+			name:          "bindError",
+			bindErr:       errors.New("bindError"),
+			serviceReturn: input.RoomOutputData{},
 			serviceErr:    nil,
 			jsonErr:       nil,
-			expected:      adapter.NewHttpError(errors.New("error"), 400),
+			expected:      adapter.NewHttpError("bindError", 400),
 		},
 		{
-			name:          "Service Error",
+			name:          "serviceError",
 			bindErr:       nil,
-			serviceOutput: boundary.RoomOutputData{},
-			serviceErr:    errors.New("error"),
+			serviceReturn: input.RoomOutputData{},
+			serviceErr:    errors.New("serviceError"),
 			jsonErr:       nil,
-			expected:      adapter.NewHttpError(errors.New("error"), 400),
+			expected:      adapter.NewHttpError("serviceError", 400),
 		},
 		{
-			name:          "context.JSON Error",
+			name:          "JSONError",
 			bindErr:       nil,
-			serviceOutput: roomOutputData,
+			serviceReturn: roomOutputData,
 			serviceErr:    nil,
-			jsonErr:       errors.New("error"),
-			expected:      adapter.NewHttpError(errors.New("error"), 500),
+			jsonErr:       errors.New("JSONError"),
+			expected:      adapter.NewHttpError("JSONError", 500),
 		},
 	}
 
-	for _, test := range testCases {
-		context := adapter.FakeHttpContext{
-			FakeBind: func(i interface{}) error { return test.bindErr },
-			FakeJSON: func(int, interface{}) error { return test.jsonErr },
-		}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			context := mocks.FakeHttpContext{
+				FakeBind: func(i interface{}) error { return tc.bindErr },
+				FakeJSON: func(int, interface{}) error { return tc.jsonErr },
+			}
 
-		service := fakeRoomService{
-			fakeCreate: func(boundary.RoomInputData) (boundary.RoomOutputData, error) {
-				return test.serviceOutput, test.serviceErr
-			},
-		}
-		controller := NewRoomController(service)
+			service := fakeRoomService{
+				fakeCreate: func(input.RoomInputData) (input.RoomOutputData, error) {
+					return tc.serviceReturn, tc.serviceErr
+				},
+			}
+			controller := NewRoomController(service)
 
-		if err := controller.Create(context); !reflect.DeepEqual(test.expected, err) {
-			t.Errorf("%v: controller.Index expected = %v, got = %v", test.name, test.expected, err)
-		}
+			if err := controller.Create(context); !reflect.DeepEqual(tc.expected, err) {
+				t.Errorf("%v: controller.Index expected = %v, got = %v", tc.name, tc.expected, err)
+			}
+		})
 	}
 }
