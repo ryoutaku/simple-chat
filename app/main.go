b12534de74224d960ba1c381f7b593d2594e4481
ryoutaku nakagawa <uoyrutakoys@gmail.com>
2021-02-17 23:31:35 +0900

    [ADD]mainパッケージ作成

--- app/main.go
+++ app/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"os"
+
+	"github.com/ryoutaku/simple-chat/app/di"
+
 	"github.com/ryoutaku/simple-chat/app/infra"
 )
 
 func main() {
-	infra.InitConfig()
-	db := infra.NewDB()
-	infra.InitRouting(db)
+	db := infra.NewDBHandler()
+	container := di.NewContainer(db)
+	router := infra.NewRouter(container)
+
+	port := ":" + os.Getenv("HTTP_PORT")
+	router.Start(port)
 }
